1292dfaa97b80d4ae29670667fe9b08412c4a425
Christopher Lamm <chris@theantichris.com>
2020-05-04 14:13:13 -0400

    added yard converter

--- length_converter.ex
+++ length_converter.ex
@@ -1,13 +1,12 @@
 defmodule MeterToLengthConverter do
-    defmodule Feet do
-        def convert(meter) do
-            meter * 3.28084
-        end
+
+    def convert(:feet, meter) do
+        meter * 3.28084
     end
 
-    defmodule Inch do
-        def convert(meter) do
-            meter * 39.3701
-        end
+    def convert(:inch, meter) do
+        meter * 39.3701
     end
+
+    def convert(:yard, meter), do: meter * 1.09361
 end
