5edbcbbe1ec132ca440339cea19ff857f1daf8c1
Christopher Lamm <chris@theantichris.com>
2020-05-04 14:22:31 -0400

    ensuring non-negative numbers

--- length_converter.ex
+++ length_converter.ex
@@ -1,12 +1,5 @@
 defmodule MeterToLengthConverter do
-
-    def convert(:feet, meter) do
-        meter * 3.28084
-    end
-
-    def convert(:inch, meter) do
-        meter * 39.3701
-    end
-
-    def convert(:yard, meter), do: meter * 1.09361
+    def convert(:feet, meter) when is_number(meter) and meter >= 0, do: meter * 3.28084
+    def convert(:inch, meter) when is_number(meter) and meter >= 0, do: meter * 39.3701
+    def convert(:yard, meter) when is_number(meter) and meter >= 0, do: meter * 1.09361
 end
